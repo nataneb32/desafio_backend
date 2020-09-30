31a8db29b13d622b0dc9bc03c82e7c522b485fd5
nataneb32 <nataestanislaubastos@gmail.com>
2020-09-30 17:57:57 -0300

    adding checking repo interface

---
+++ pkg/checkins/repo.go
@@ -0,0 +1,9 @@
+package checkins
+
+type CheckInRepo interface {
+	GetCheckIn(*CheckIn) error
+	//	ListGuests(Guest, uint, uint) (error, []Guest)
+	//	GetAllGuest(Guest) (error, Guest)
+	CreateCheckIn(*CheckIn) error
+	//	DeleteGuest(Guest) error
+}
